17445848eb6de06617a6af18f74df46c8c840ef5
ismv00 <ismv00@icloud.com>
2023-08-14 15:37:41 -0400

    feat:create a firts constrain and screen navigations

--- iQuiz/ViewController.swift
+++ iQuiz/ViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var MostrarBotao: UIButton!
+    
+    @IBAction func botaoIniciar(_ sender: Any) {
+        print("Botão foi pressionado")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        ajustarBotao()
         // Do any additional setup after loading the view.
+    }
+    
+    func ajustarBotao() {
+        MostrarBotao.layer.cornerRadius = 12.0
     }
 
 
